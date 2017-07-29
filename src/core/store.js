1982b520e8ac4c09dd9b520e0a4a49de8916e645
xrbarney <xrbarney@gmail.com>
2017-07-29 14:45:19 +0300

    Code cleanup

--- src/core/store.js
+++ src/core/store.js
@@ -2,9 +2,6 @@ import {createStore, applyMiddleware} from "redux";
 import {composeWithDevTools} from "remote-redux-devtools";
 import reducer from "../reducers/reducer";
 import thunk from 'redux-thunk'
-const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
-
-
 
 export default function configureStore(initialState) {
 
@@ -12,15 +9,6 @@ export default function configureStore(initialState) {
     const store = createStore(reducer, initialState, composeEnhancers(
         applyMiddleware(thunk)
     ));
-
-    // if (module.hot) {
-    //     // Enable Webpack hot module replacement for reducers
-    //     module.hot.accept('../reducers', () => {
-    //         const nextReducer = require('../reducers').default;
-    //         store.replaceReducer(nextReducer);
-    //     });
-    // }
-
     return store;
 }
 
