455966e48d5080f3e928443c2e203b7e2aa1e0d2
xrbarney <xrbarney@gmail.com>
2017-12-02 18:22:39 +0200

    remove redux dev tools

--- src/core/store.js
+++ src/core/store.js
@@ -1,14 +1,8 @@
 import {createStore, applyMiddleware} from "redux";
-import {composeWithDevTools} from "remote-redux-devtools";
 import reducer from "../reducers/reducer";
 import thunk from 'redux-thunk'
 
 export default function configureStore(initialState) {
-
-    const composeEnhancers = composeWithDevTools({ realtime: true });
-    const store = createStore(reducer, initialState, composeEnhancers(
-        applyMiddleware(thunk)
-    ));
-    return store;
+    return createStore(reducer, initialState,  applyMiddleware(thunk));
 }
 
