e47797e453f66e5789d3a037d9fb3f409c6bedfc
xrbarney <xrbarney@gmail.com>
2017-12-08 11:06:47 +0200

    add redux remote devtools support

--- src/core/store.js
+++ src/core/store.js
@@ -1,8 +1,13 @@
 import {createStore, applyMiddleware} from "redux";
 import reducer from "../reducers/reducer";
+import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk'
 
 export default function configureStore(initialState) {
-    return createStore(reducer, initialState,  applyMiddleware(thunk));
+    const store = createStore(reducer, initialState, composeWithDevTools(
+        applyMiddleware(thunk),
+        // other store enhancers if any
+      ));
+    return store;
 }
 
